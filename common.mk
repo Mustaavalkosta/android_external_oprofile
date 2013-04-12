587cda929c812798afae0dbe61f52187e0a2fb1e
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-04-12 00:00:33 +0000

    Revert "[TEMP] don't set HAVE_LIBBFD true if using linaro's prebuilt toolchain"
    This reverts commit 2944aee47e95b2152d4e384f75bb91bfe36814ac.

--- common.mk
+++ common.mk
@@ -49,9 +49,7 @@ common_host_cflags += -DMISSING_MREMAP
 common_host_ldlibs_libiconv := -liconv
 else
 ifeq ($(HOST_OS)-$(HOST_ARCH),linux-x86)
-ifeq ($(findstring android-toolchain-eabi,$(TARGET_TOOLS_PREFIX)),)
 HAVE_LIBBFD := true
-endif
 endif
 endif
 endif
